98674e9ed577b92588e6a006067f41b22860eab7
Sriraj Bura <sriraj.bura16@gmail.com>
2021-07-28 17:21:46 -0400

    implemented database and static models

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const coolUserRouter = require('./routes/cool');
 
 var app = express();
 
+//set up mongoose connection
+const mongoose = require('mongoose');
+const mongoDB =
+  'mongodb+srv://local-library:deved123@cluster0.vyhb8.mongodb.net/local_library?retryWrites=true&w=majority';
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
