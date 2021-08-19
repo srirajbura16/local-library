b210fceeade6b5b5a09431bcb1d910a616df86d9
Sriraj Bura <sriraj.bura16@gmail.com>
2021-08-19 16:49:00 -0400

    implemented bookinstance delete form

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,6 +1,6 @@
 var BookInstance = require('../models/bookinstance');
 var Book = require('../models/book');
-
+var async = require('async');
 const { body, validationResult } = require('express-validator');
 
 // Display list of all BookInstances.
@@ -113,12 +113,37 @@ exports.bookinstance_create_post = [
 ];
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance delete GET');
+  res.render('bookinstance_delete', {
+    title: 'Delete BookInstance',
+    bookinstanceID: req.params.id,
+  });
 };
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance delete POST');
+  async.parallel(
+    {
+      bookinstance: function (callback) {
+        BookInstance.findById(req.params.id).exec(callback);
+      },
+    },
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      // Success
+      BookInstance.findByIdAndRemove(
+        req.body.bookinstanceid,
+        function deleteBookInstance(err) {
+          if (err) {
+            return next(err);
+          }
+          // Success - go to author list
+          res.redirect('/catalog/books');
+        }
+      );
+    }
+  );
 };
 
 // Display BookInstance update form on GET.
